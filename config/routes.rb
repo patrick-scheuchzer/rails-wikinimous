820598b19ddf7c21ae32e2179242fb3205fbc897
Patrick Scheuchzer <scheuchzer.patrick@gmail.com>
2017-04-18 17:16:15 +0200

    added user login that is required to add, edit, or delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+
+  root to: 'articles#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
+
+  get 'signup', to: 'users#new'
+
+  post 'users', to: 'users#create'
+
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
